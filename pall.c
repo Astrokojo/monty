fd1eb7c86adffaa41a5671c3f9d3772049fa66cc
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-10-20 01:18:40 +0000

    fix

--- pall.c
+++ pall.c
@@ -2,18 +2,13 @@
 /**
  * pall - prints the whole stack
  * @head: head of the stack
- * @line_number: unused
  * Return: returns nothing
  */
-void pall(stack_t **head, unsigned int line_number)
+void pall(stack_t **head)
 {
-	(void)counter;
 	stack_t *temp;
 
 	temp = *head;
-
-	if temp == NULL
-		return;
 
 	while (temp)
 	{
